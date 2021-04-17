836328d45992ae124b320f798888be428e28e470
Diego Sepúlveda Millán <41056207+DiegoSepuMillan@users.noreply.github.com>
2021-04-17 12:05:56 +0200

    Update setas.pl

--- setas.pl
+++ setas.pl
@@ -2,3 +2,14 @@ ask(A, V):- known(yes, A, V),!.
 ask(A, V):- known(_, A, V),!,fail.
 ask(A, V):- write(A:V), write('? : '), read(Y), 
   asserta(known(Y, A, V)), Y == yes.
+
+seta(agaricus_campester):-
+    sombrero(blanco),(tono(grisaceo);tono(rosaceo)),
+    (laminas(blanco_rosaceo);laminas(marron)),
+    temporada(otoño),temporada(primavera), olor(agradable).
+
+seta(amanita_caesarea):-
+    sombrero(naranja),tono(amarillento),
+    (laminas(amarillo_palido);laminas(amarillo_dorado)),
+     temporada(otoño).
+
