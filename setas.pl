bc6f460724b52ac898d427d1412d9f5092d1905b
Diego Sepúlveda Millán <41056207+DiegoSepuMillan@users.noreply.github.com>
2021-04-17 12:35:07 +0200

    Update setas.pl

--- setas.pl
+++ setas.pl
@@ -3,13 +3,42 @@ ask(A, V):- known(_, A, V),!,fail.
 ask(A, V):- write(A:V), write('? : '), read(Y), 
   asserta(known(Y, A, V)), Y == yes.
 
+¡%setsa no venenosas%
+
 seta(agaricus_campester):-
-    sombrero(blanco),(tono(grisaceo);tono(rosaceo)),
+     sombrero_forma(concava),sombrero(blanco),(tono(grisaceo);tono(rosaceo)),
     (laminas(blanco_rosaceo);laminas(marron)),
     temporada(otoño),temporada(primavera), olor(agradable).
 
 seta(amanita_caesarea):-
-    sombrero(naranja),tono(amarillento),
+    sombrero_forma(concava),sombrero(naranja),tono(amarillento),
     (laminas(amarillo_palido);laminas(amarillo_dorado)),
      temporada(otoño).
+
+seta(boletus_edulis):-
+     sombrero_forma(concava),sombrero(marron),tono(blanquecino),temporada(verano),
+    cuticula(viscosa).
+
+seta(lactarius_deliciosus):-
+    sombrero_forma(convexa),sombrero(naranja),tono(rojizo),
+    laminas(naranjas),manchas(verdes),
+    (temporada(otoño);temporada(invierno)).
+
+seta(cantharellus_cibarius):-
+    sombrero_forma(convexa),sombrero(amarillo),tono(blanquecino),
+    laminas(amarillo_palido),temporada(otoño).
+
+%setas venenosas%
+
+seta(amanita_phalloides):-
+    sombrero_forma(concava),sombrero(blanco),tono(verde),
+    laminas(blancas),(temporada(invierno);temporada(primavera);
+                     temporada(verano);temporada(otoño)).
+
+seta(amanita_verna):-
+    sombrero_forma(plato),sombrero(blanco), tono(blanquecino).
+
+seta(amanita_muscaria):-
+    sombrero_forma(plato),sombrero(rojo),manchas(blancas),
+    laminas(blancas).
 
